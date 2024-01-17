cbcd482e1a7f77916ffdbd86fb6c783ee1cab72a
henzzo <henokgetachew500@gmail.com>
2024-01-17 21:02:01 +0300

    feat: lint fix

--- src/app/api/payment-intent/route.ts
+++ src/app/api/payment-intent/route.ts
@@ -3,10 +3,7 @@ import { Stripe } from 'stripe';
 export const runtime = 'edge';
 
 export async function GET() {
-  const s = new Stripe(
-    // 'sk_test_51OZHVcIJ3gOeHcJFYDLVKx1W4LZ8d8YNoDQfV8l1TnImVRuh89RUprZwSF6TjQI4XoWTNR723v9iax3hkKfjaljU00GClxREZW'
-    process.env.STRIPE_SECRET ?? '',
-  );
+  const s = new Stripe(process.env.STRIPE_SECRET ?? ''); // 'sk_test_51OZHVcIJ3gOeHcJFYDLVKx1W4LZ8d8YNoDQfV8l1TnImVRuh89RUprZwSF6TjQI4XoWTNR723v9iax3hkKfjaljU00GClxREZW'
 
   const paymentIntent = await s.paymentIntents.create({
     amount: 300,
